3fee56a3aba1871e06856623beb51fa1812b07e3
Radosław Cymer <cymerrad@gmail.com>
2019-09-22 17:39:03 +0200

    Tests for new approach

--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
 module ParserSpec
   ( spec
   )
@@ -8,8 +9,27 @@ where
 import           Import
 import           Parser
 import           Test.Hspec
+import           Test.Hspec.Megaparsec
+import           Text.Megaparsec
 
+
+prs :: Text -> Either (ParseErrorBundle Text Void) LispAtom
+prs = parse lexExpr ""
+
+itShPrs description input expectedOutput =
+  it description $ prs input `shouldParse` expectedOutput
 
 spec :: Spec
 spec = describe "GL & HF testing this" $ do
-  it "fml" $ shouldBe True True
+  primitivesSpec
+  combinationsSpec
+
+primitivesSpec :: Spec
+primitivesSpec = describe "Parsing atoms" $ do
+  it "empty list" $ prs "()" `shouldParse` List []
+  it "string" $ prs "\"string\"" `shouldParse` Constant (String "string")
+  itShPrs "integer" "1337" (Constant . Number . Integral 1337)
+
+combinationsSpec :: Spec
+combinationsSpec = describe "Parsing some combinations" $ do
+  it "should work" $ shouldBe True True
