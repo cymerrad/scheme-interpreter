46ac786d6294d45ca68755f42539db3eab8e2ddb
Radosław Cymer <cymerrad@gmail.com>
2019-10-04 10:51:46 +0200

    brb

--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -11,6 +11,7 @@ import           Parser
 import           Test.Hspec
 import           Test.Hspec.Megaparsec
 import           Text.Megaparsec
+import           Prelude                        ( uncurry )
 
 
 prs :: String -> Either (ParseErrorBundle String Void) LispExpr
@@ -33,15 +34,21 @@ spec = describe "GL & HF testing this" $ do
 
 primitivesSpec :: Spec
 primitivesSpec = describe "Parsing atoms" $ do
-  it "empty list" $ prs "()" `shouldParse` List []
-  it "string" $ prs "\"string\"" `shouldParse` (lString "string")
-  itShPrs "integer"         "1337"       (lNum (Integral 1337))
-  itShPrs "boolean true"    "#t"         (lBool True)
-  itShPrs "boolean false"   "#f"         (lBool False)
-  itShPrs "character \\n"   "#\\newline" (lChar '\n')
-  itShPrs "character space" "#\\space"   (lChar ' ')
-  itShPrs "character a"     "#\\a"       (lChar 'a') -- TODO: https://hspec.github.io/quickcheck.html
+  itShPrs "empty list"          "() "            (List [])
+  itShPrs "string"              "\"string\" "    (lString "string")
+  itShPrs "integer"             "1337 "          (lNum (Integral 1337))
+  itShPrs "binary integer"      "#b10100111001 " (lNum (Integral 1337))
+  itShPrs "octal integer"       "#o2471 "        (lNum (Integral 1337))
+  itShPrs "decimal integer"     "#d1337 "        (lNum (Integral 1337))
+  itShPrs "hexadecimal integer" "#x539 "         (lNum (Integral 1337))
+  itShPrs "boolean true"        "#t "            (lBool True)
+  itShPrs "boolean false"       "#f "            (lBool False)
+  itShPrs "character \\n"       "#\\newline "    (lChar '\n')
+  itShPrs "character space"     "#\\space "      (lChar ' ')
+  itShPrs "character a"         "#\\a "          (lChar 'a') -- TODO: https://hspec.github.io/quickcheck.html
   itShNotPrs "character duplicated a" "#\\aa"
+  itShNotPrs "almost a boolean true"  "#tt"
+  itShNotPrs "almost a boolean false" "#ff"
 
 
 abbreviationsSpec :: Spec
@@ -49,7 +56,9 @@ abbreviationsSpec = describe "Parsing abbreviations" $ do
   itShPrs "quote empty list"      "'()" (Quote (DList []))
   itShPrs "quasiquote empty list" "`()" (Quasiquote (DList []))
 
+
 compoundSpec :: Spec
 compoundSpec = describe "Parsing some combinations" $ do
   it "should work" $ shouldBe True True
   it "shouldn't suggest not using the 'do' notation" $ True `shouldBe` True
+
